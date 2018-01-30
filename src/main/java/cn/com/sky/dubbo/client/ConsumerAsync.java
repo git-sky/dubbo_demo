68ab87ef5e23b9ceae2f7763d2faeb03f326240a
git-sky <linkme2008@sina.com>
2018-01-30 14:40:47 +0800

    add

--- src/main/java/cn/com/sky/dubbo/client/ConsumerAsync.java
+++ src/main/java/cn/com/sky/dubbo/client/ConsumerAsync.java
@@ -43,26 +43,31 @@ public class ConsumerAsync {
 		});
 
 		DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
-		long begin = System.currentTimeMillis();
-		System.out.println("begin:" + begin);
-		String result = demoService.sayHello("world"); // 执行远程方法
-		System.out.println("end:" + (System.currentTimeMillis() - begin));
-		System.out.println(result); // 显示调用结果
-
-		Future<String> fooFuture = RpcContext.getContext().getFuture(); // 拿到调用的Future引用，当结果返回后，会被通知和设置到此Future。
-		String s = fooFuture.get();
-		System.out.println(s);
-
-		HelloAction helloAction = (HelloAction) context.getBean("helloAction"); // 获取远程服务代理
-		long begin2 = System.currentTimeMillis();
-		System.out.println("begin:" + begin2);
-		String result2 = helloAction.say("world"); // 执行远程方法
-		System.out.println("end:" + (System.currentTimeMillis() - begin2));
-		System.out.println(result2); // 显示调用结果
-
-		Future<String> fooFuture2 = RpcContext.getContext().getFuture(); // 拿到调用的Future引用，当结果返回后，会被通知和设置到此Future。
-		String s2 = fooFuture2.get();
-		System.out.println(s2);
+		while (true) {
+			Thread.sleep(3000);
+			long begin = System.currentTimeMillis();
+			System.out.println("begin:" + begin);
+			String result = demoService.sayHello("world"); // 执行远程方法
+			System.out.println("end:" + (System.currentTimeMillis() - begin));
+			System.out.println(result); // 显示调用结果
+		}
+		//
+		// Future<String> fooFuture = RpcContext.getContext().getFuture(); //
+		// 拿到调用的Future引用，当结果返回后，会被通知和设置到此Future。
+		// String s = fooFuture.get();
+		// System.out.println(s);
+		//
+		// HelloAction helloAction = (HelloAction) context.getBean("helloAction"); // 获取远程服务代理
+		// long begin2 = System.currentTimeMillis();
+		// System.out.println("begin:" + begin2);
+		// String result2 = helloAction.say("world"); // 执行远程方法
+		// System.out.println("end:" + (System.currentTimeMillis() - begin2));
+		// System.out.println(result2); // 显示调用结果
+		//
+		// Future<String> fooFuture2 = RpcContext.getContext().getFuture(); //
+		// 拿到调用的Future引用，当结果返回后，会被通知和设置到此Future。
+		// String s2 = fooFuture2.get();
+		// System.out.println(s2);
 
 	}
 
